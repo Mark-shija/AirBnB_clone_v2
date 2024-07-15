7077a95a986b21b14a4ba2dc1b1577abef8ca99b
Mark-shija <plusmaxtechtz@gmail.com>
2024-07-15 16:48:53 -0700

    Compress before sending folder to servers

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,25 +1,40 @@
 #!/usr/bin/python3
-""" This module contains the 
-function do_pack that generates a .tgz archive
-from the contents of the web_static folder 
-(fabric script) """
+"""
+This module contains the function do_pack that generates a .tgz archive
+from the contents of the web_static folder (fabric script).
+"""
 
-from fabric.api import *
+from fabric.api import local
 from datetime import datetime
 import os
 
 def do_pack():
-    local("sudo mkdir -p versions")
+    """
+    Generates a .tgz archive from the contents of the web_static folder.
 
+    The function creates a 'versions' directory if it does not exist, 
+    generates a timestamped archive of the web_static folder, 
+    and returns the path to the created archive.
+
+    Returns:
+    str: The path to the created .tgz archive.
+    """
+    # Create versions directory if it does not exist
+    local("mkdir -p versions")
+
+    # Generate a timestamp string for the archive name
     t = datetime.now()
-    t_str = t.strftime('%Y%m%d%H%M%s')
+    t_str = t.strftime('%Y%m%d%H%M%S')
 
-    local(f'sudo tar -cvzf versions/web_static_{t_str}.tgz web_static')
+    # Create the .tgz archive
+    archive_path = f'versions/web_static_{t_str}.tgz'
+    local(f'tar -cvzf {archive_path} web_static')
 
-    f_path = f"versions/web_static_{t_str}.tgz"
+    # Get the size of the created archive
+    archive_size = os.path.getsize(archive_path)
 
-    f_size = os.path.getsize(f_path)
+    # Print details about the created archive
+    print(f'web_static packed: {archive_path} -> {archive_size}Bytes')
 
-    print(f'web_static packed:{f_path} -> {f_size}Bytes')
-
-    return (f_path)
+    # Return the path to the created archive
+    return archive_path
