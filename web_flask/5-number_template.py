00f1f246b5af8497b145564e40f86a398277c4cb
Mark-shija <plusmaxtechtz@gmail.com>
2024-07-31 14:20:41 -0700

    render templates in flask

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -38,7 +38,7 @@ def imanumber(n):
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def numbersandtemplates(n):
+def numtemplates(n):
     """ render templates only if n is an integer"""
     return render_template('5-number.html', n=n)
 
